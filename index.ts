7e6be0d6e88ee80210551bfb7c1f286fbe38aff7
Joshua Caron <jocaron@gmail.com>
2020-08-02 15:53:53 -0400

    Exporting createAction helper function and bumping version.

--- index.ts
+++ index.ts
@@ -7,6 +7,7 @@ export {useState} from './lib/useState.js';
 export {useReducer, useObjectReducer} from './lib/useReducer.js';
 export {useEffect, useLayoutEffect} from './lib/useEffect.js';
 export {useMemo} from './lib/useMemo.js';
+export {createAction} from './lib/createAction.js';
 
 export function unhooked<T>(render: (data: T, root: HTMLElement | ShadowRoot) => void, defaultOptions?: Options) {
   return _componentCreator<T>((instance, data, userOptions) => {
